fd11afd8994e4f2fab189406d0052c20e95018ce
UrMjsty <alexciornyi@gmail.com>
2024-05-13 00:52:27 +0300

    inventory update

--- Assets/Scripts/InventoryClass.cs
+++ Assets/Scripts/InventoryClass.cs
@@ -5,20 +5,27 @@ using UnityEngine;
 
 public struct Inventory
 {
-    private int goldAmount;
-    private int goldIngotsAmount;
-    private int silverAmount;
-    private int silverIngotsAmount;
-    private int diamondAmount;
-    private int diamondShapedAmount;
-    private int rubyAmount;
-    private int rubyShapedAmount;
-    private int sapphireAmount;
-    private int sapphireShapedAmount;
-    private int emeraldAmount;
-    private int emeraldShapedAmount;
-    private int amethystAmount;
-    private int amethystShapedAmount;
+    public int Money;
+    public int GoldAmount;
+    public int GoldIngotsAmount;
+    public int SilverAmount;
+    public int SilverIngotsAmount;
+    public int DiamondAmount;
+    public int DiamondShapedAmount;
+    public int RubyAmount;
+    public int RubyShapedAmount;
+    public int SapphireAmount;
+    public int SapphireShapedAmount;
+    public int EmeraldAmount;
+    public int EmeraldShapedAmount;
+    public int AmethystAmount;
+    public int AmethystShapedAmount;
+
+    public int Space => DiamondAmount + GoldAmount + SilverAmount + AmethystAmount + EmeraldAmount + RubyAmount +
+                        SapphireAmount;
+
+    public int MaxSpace;
+
 }
 
 public enum Ore
@@ -36,7 +43,7 @@ public enum Gem
     Amethyst
 }
 
-public enum JewelryType
+public enum JewelryType 
 {
     Necklace,
     Ring,
@@ -58,6 +65,26 @@ public class Jewelry
         ore = o;
         gem = g;
         type = t;
+    }
+
+    public int GetPrice()
+    {
+        return price;
+    }
+
+    public JewelryType GetJewelryType()
+    {
+        return type;
+    }
+
+    public Ore GetOre()
+    {
+        return ore;
+    }
+
+    public Gem GetGem()
+    {
+        return gem;
     }
 }
 
@@ -153,9 +180,238 @@ public class InventoryClass : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        inventory = new Inventory();
+        inventory = new Inventory
+        {
+            MaxSpace = 10
+        };
     }
 
+    public void Dig(object obj)
+    {
+        switch (obj.ToString())
+        {
+            case "Diamond":
+                {
+                    inventory.DiamondAmount++;
+                }
+                break;
+            case 
+                "Ruby":
+                {
+                    inventory.RubyAmount++;
+                }
+                break;
+            case 
+                "Sapphire":
+                {
+                    inventory.SapphireAmount++;
+                }
+                break;
+            case 
+                "Emerald":
+                {
+                    inventory.EmeraldAmount++;
+                }
+                break;
+            case 
+                "Amethyst":
+                {
+                    inventory.AmethystAmount++;
+                }
+                break;
+            case 
+                "Gold":
+                {
+                    inventory.GoldAmount++;
+                }
+                break;
+            case "Silver":
+                {
+                    inventory.SilverAmount++;
+                }
+                break;
+        }
+
+    }
+
+    public void Sell(Jewelry jew)
+    {
+        if (availableJewelry.Contains(jew))
+        {
+            availableJewelry.Remove(jew);
+            inventory.Money += jew.GetPrice();
+        }   
+    }
+    public void Craft(object obj)
+    {
+        if (obj.GetType() == typeof(Jewelry))
+        {
+            Jewelry jew = (Jewelry)obj;
+            switch (jew.GetJewelryType())
+            {
+                case JewelryType.Necklace:
+                    if (GetShapedGemAmount(jew.GetGem()) >= 5 && GetOreIngotsAmount(jew.GetOre()) >= 3)
+                    {
+                        RemovePieces(jew);
+                        availableJewelry.Add(jew);
+                    }
+                    break;
+                case JewelryType.Ring:
+                    if (GetShapedGemAmount(jew.GetGem()) >= 2 && GetOreIngotsAmount(jew.GetOre()) >= 2)
+                    {
+                        RemovePieces(jew);
+                        availableJewelry.Add(jew);
+                    }
+                    break;
+                case JewelryType.Earrings:
+                    if (GetShapedGemAmount(jew.GetGem()) >= 4 && GetOreIngotsAmount(jew.GetOre()) >= 2)
+                    {
+                        RemovePieces(jew);
+                        availableJewelry.Add(jew);
+                    }
+                    break;
+                default:
+                    throw new ArgumentOutOfRangeException();
+            }
+        }
+        switch (obj.ToString())
+        {
+            case "Diamond":
+                if (inventory.DiamondAmount > 0)
+                {
+                    inventory.DiamondAmount--;
+                    inventory.DiamondShapedAmount += 3;
+                }
+                break;
+                case 
+                "Ruby":
+                    if (inventory.RubyAmount > 0)
+                    {
+                        inventory.RubyAmount--;
+                        inventory.RubyShapedAmount += 3;
+                    }
+                    break;
+               case 
+                   "Sapphire":
+                   if (inventory.SapphireAmount > 0)
+                   {
+                       inventory.SapphireAmount--;
+                       inventory.SapphireShapedAmount += 3;
+                   }
+                   break;
+                case 
+                   "Emerald":
+                    if (inventory.EmeraldAmount > 0)
+                    {
+                        inventory.EmeraldAmount--;
+                        inventory.EmeraldShapedAmount += 3;
+                    }
+                break;
+                case 
+                "Amethyst":
+                    if (inventory.AmethystAmount > 0)
+                    {
+                        inventory.AmethystAmount--;
+                        inventory.AmethystShapedAmount += 3;
+                    }
+                    break;
+                case 
+                "Gold":
+                    if (inventory.GoldAmount > 0)
+                    {
+                        inventory.GoldAmount--;
+                        inventory.GoldIngotsAmount += 2;
+                    }
+                    break;
+                case "Silver":
+                if (inventory.SilverAmount > 0)
+                {
+                    inventory.SilverAmount--;
+                    inventory.SilverIngotsAmount += 2;
+                }
+                break;
+        }
+    }
+
+    private int GetGemAmount(Gem gem)
+    {
+        switch (gem)
+        {
+            case Gem.Diamond:
+                return inventory.DiamondAmount;
+                break;
+            case Gem.Ruby:
+                return inventory.RubyAmount;
+                break;
+            case Gem.Sapphire:
+                return inventory.SapphireAmount;
+                break;
+            case Gem.Emerald:
+                return inventory.EmeraldAmount;
+                break;
+            case Gem.Amethyst:
+                return inventory.AmethystAmount;
+                break;
+            default:
+                throw new ArgumentOutOfRangeException(nameof(gem), gem, null);
+        }
+    }
+
+    private void RemovePieces(Jewelry jew)
+    {
+        
+    }
+    private int GetShapedGemAmount(Gem gem)
+    {
+        switch (gem)
+        {
+            case Gem.Diamond:
+                return inventory.DiamondShapedAmount;
+                break;
+            case Gem.Ruby:
+                return inventory.RubyShapedAmount;
+                break;
+            case Gem.Sapphire:
+                return inventory.SapphireShapedAmount;
+                break;
+            case Gem.Emerald:
+                return inventory.EmeraldShapedAmount;
+                break;
+            case Gem.Amethyst:
+                return inventory.AmethystShapedAmount;
+                break;
+            default:
+                throw new ArgumentOutOfRangeException(nameof(gem), gem, null);
+        }
+    }
+    private int GetOreAmount(Ore ore)
+    {
+        switch (ore)
+        {
+            case Ore.Silver:
+                return inventory.SilverAmount;
+                break;
+            case Ore.Gold:
+                return inventory.SilverAmount;
+                break;
+            default:
+                throw new ArgumentOutOfRangeException(nameof(ore), ore, null);
+        }
+    } 
+    private int GetOreIngotsAmount(Ore ore)
+    {
+        switch (ore)
+        {
+            case Ore.Silver:
+                return inventory.SilverIngotsAmount;
+                break;
+            case Ore.Gold:
+                return inventory.GoldIngotsAmount;
+                break;
+            default:
+                throw new ArgumentOutOfRangeException(nameof(ore), ore, null);
+        }
+    }
     // Update is called once per frame
     void Update()
     {
