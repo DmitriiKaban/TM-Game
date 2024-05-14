ec0b5193bb9048f3197729baf6202a5b86b45259
mirrerror <cayote20@mail.ru>
2024-05-14 12:07:03 +0300

    add id field for npcs

--- Assets/Scripts/NPCController.cs
+++ Assets/Scripts/NPCController.cs
@@ -5,10 +5,11 @@ using UnityEngine;
 public class NPCController : MonoBehaviour, Interactable
 {
     [SerializeField] Dialog dialog;
+    [SerializeField] string id;
+    
     public void Interact()
     {
         Debug.Log("Interact with NPC!!!!!");
         StartCoroutine(DialogManager.Instance.ShowDialog(dialog));
-        
     }
 }
